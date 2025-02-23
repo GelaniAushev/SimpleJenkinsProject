d6218c05747cfba1448ed43c22fd0bda2f0a8486
Gelani Aushev <aushevgelani006@gmail.com>
2025-02-23 16:19:34 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.printf("Hello and welcome!");
+            Thread.sleep(2000);
+        }
+
+    }
+}
